aa24fea765e49e28c983165fdda350dbec41335b
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-09-08 21:35:03 -0400

    Created navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,30 @@
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./constants";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./pages/navbar";
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
-    </div>
+    <Box className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box height="100%" width="100%" padding="1rem 2rem 4rem 2rem ">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions Page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </Box>
   );
 };
 
