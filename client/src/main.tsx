eb6c6b1c958052ae93f2778f306d3b8660b07547
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-09-14 15:51:50 -0400

    Setup redux toolkit store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "./state/api.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (defaultMiddleware) => defaultMiddleware().concat(api.middleware),
+});
+
+setupListeners(store.dispatch);
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
